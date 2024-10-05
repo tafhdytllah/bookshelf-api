cc3e2a9af855f1e090e7006dedccac0b8467a37e
Taufik Hidayatullah <taufikhh.97@gmail.com>
2024-10-05 13:15:17 +0700

    config Hapi server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,18 @@
-console.log("Hello test");
+const Hapi = require("@hapi/hapi");
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 9000,
+    host: "localhost",
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
+  });
+
+  await server.start();
+  console.log(`Server running on ${server.info.uri}`);
+};
+
+init();
